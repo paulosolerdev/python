a5e7a5e7a0ed2d6d614ca1a15dcdc581b3e582bd
Paulo Soler <paulosolerdev@gmail.com>
2024-07-13 13:30:07 -0300

    Update meu_sistema.py

--- use-a-cabeca/meu_sistema.py
+++ use-a-cabeca/meu_sistema.py
@@ -3,7 +3,17 @@
 """
 
 import sys
+import os
 
 print(f"Este é o meu sistema operacional: {sys.platform.title()}") # Sistema Operacional
 
 print(f"Esta é a versão do meu Python: {sys.version.title()}")  # Versão do Python
+
+print(f"Esta é minha localização: {os.getcwd()}")
+
+# Variáveis de ambiente em conjunto
+print(f"{os.environ}")
+
+# Variáveis de ambiente individualmente (especificamente)
+print(f"{os.getenv('HOME')}")
+
