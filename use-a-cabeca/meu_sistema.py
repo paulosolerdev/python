d3bbec35b62a3a5db3d22a6a918db6e5e3a7ab35
Paulo Soler <paulosolerdev@gmail.com>
2024-07-13 13:24:16 -0300

    Create meu_sistema.py

---
+++ use-a-cabeca/meu_sistema.py
@@ -0,0 +1,9 @@
+"""
+    Exibe informações do meu Sistema Operacional.
+"""
+
+import sys
+
+print(f"Este é o meu sistema operacional: {sys.platform.title()}") # Sistema Operacional
+
+print(f"Esta é a versão do meu Python: {sys.version.title()}")  # Versão do Python
