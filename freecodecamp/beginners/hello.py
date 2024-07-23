99e3558fbbc2aa4c0cbef4be5491cbed98f71bbe
Paulo Soler <paulosolerdev@gmail.com>
2024-07-23 12:23:22 -0300

    Update hello.py

--- freecodecamp/beginners/hello.py
+++ freecodecamp/beginners/hello.py
@@ -1,1 +1,18 @@
 print("Hello World")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+#================================$================================#
+
+character_name = "Paulo"
+character_age = 39
+
+print(f"Desde criança {character_name} gosta de apreciar a chuva.")
+print(f"{character_name} sente-se bem com a natureza.")
+print(f"{character_name} tem {character_age} anos.")
+print(f"Há {character_age} a mãe de {character_name} teve um bebê.")
+print(f"Este bebê chamado {character_name} teve muito amor e carinho.")
+print(f"{character_name} na época nem imaginava seu futuro.")
+print(f"Hoje {character_name} ama sua esposa e é feliz.")
