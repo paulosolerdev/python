a549049897a98ce6283238d14e5ea2adb7d70dfe
Paulo Soler <paulosolerdev@gmail.com>
2024-07-24 11:29:49 -0300

    Create fatura.py

---
+++ iniciante-avancado/inicio/fatura.py
@@ -0,0 +1,11 @@
+"""
+Primeiro programa do livro do Fernando Feltrin da série 
+do iniciante ao avançado e às redes neurais.
+"""
+
+nome = 'João'
+dia_vencimento = 10
+valor_fatura = 156.99
+
+print(f'\nOlá, caro {nome}, \nSua fatura vence dia {dia_vencimento} de janeiro')
+print(f'\nO valor é de R${valor_fatura}.\nFavor pagar até o prazo para evitar multas.')
