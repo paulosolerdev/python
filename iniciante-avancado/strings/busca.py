b1ceaf6fab89d6238183b2454f96e1595ca37490
Paulo Soler <paulosolerdev@gmail.com>
2024-07-24 12:27:48 -0300

    Create busca.py

---
+++ iniciante-avancado/strings/busca.py
@@ -0,0 +1,14 @@
+frase = 'A LINGUAGEM PYTHON É MUITO FÁCIL DE APRENDER.'
+print('py' in frase)
+
+
+frase = frase.lower()
+print('py' in frase)
+
+#==========$==========$$==========$==========#
+#==========$==========$$==========$==========#
+
+# Split
+frase = frase.lower()
+
+print(frase.split())
