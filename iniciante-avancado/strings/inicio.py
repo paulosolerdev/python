6b43387e1f51c864d1781d47dc0b6bd951a5b4d3
Paulo Soler <paulosolerdev@gmail.com>
2024-07-24 11:45:34 -0300

    Create inicio.py

---
+++ iniciante-avancado/strings/inicio.py
@@ -0,0 +1,22 @@
+frase = 'Era um dia "muuuito" frio'
+
+print(frase)
+
+#===$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$===#
+
+texto1 = """
+            Hoje tenho treino
+            Amanhã tenho riqueza
+            Quinta tenho prosperidade.
+        """
+
+print(texto1)
+
+#==========$==========$$==========$$$==========$$==========$==========#
+
+frase1 = 'A linguagem Python é muito fácil de aprender.'
+
+print(f'{frase1.lower()} "frase em minúscula (lower)"')
+print(f'{frase1.upper()} "frase em maiúscula (upper)"')
+print(f'{frase1.title()} "frase em título (title)"')
+print(f'{frase1.capitalize()} "frase em capitalize (primeira maiúscula)"')
