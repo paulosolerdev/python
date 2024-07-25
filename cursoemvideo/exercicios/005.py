e787eccda3213fc2addc4966ac0f39f8487e6c2c
Paulo Soler <paulosolerdev@gmail.com>
2024-07-25 10:18:55 -0300

    Create 005.py

---
+++ cursoemvideo/exercicios/005.py
@@ -0,0 +1,11 @@
+"""num = int(input(f'Informe um número inteiro: '))
+a = num - 1
+s = num + 1
+
+print(f'Analisando o valor {num}, seu antecessor é {a} e o sucessor é {s}')
+"""
+
+#=========================================#
+
+n = int(input(f'Informe um número inteiro: '))
+print(f'O valor é {n}, o antecessor é {n-1}, e o sucessor é {n+1}')
