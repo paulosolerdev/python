ac7ed7c63b10a28c483fe02abcc6efe34f02d8d9
Paulo Soler <paulosolerdev@gmail.com>
2024-07-25 09:44:08 -0300

    Update pi.py

--- iniciante-avancado/strings/pi.py
+++ iniciante-avancado/strings/pi.py
@@ -1,3 +1,10 @@
-from math import pi
+"""from math import pi
 
-print(f'O número pi é: {pi}')
+print(f'O número pi é: {pi:.2f}')"""
+
+#===$======$======$$======$======$===#
+#===$======$======$$======$======$===#
+
+num = 34.29593875612780
+ 
+print(f'O valor da ação fechou em {num:.2f}.')
