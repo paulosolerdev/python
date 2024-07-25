126dad474e9869a484deaf26218481860b4749da
Paulo Soler <paulosolerdev@gmail.com>
2024-07-25 19:46:40 -0300

    Create 006.py

---
+++ cursoemvideo/exercicios/006.py
@@ -0,0 +1,15 @@
+import math
+
+num = int(input(f"Informe um número: "))
+
+print(f"O dobro de {num} é igual a: {num * 2}")
+print(f'O triplo de {num} é igual a: {num * 3}')
+
+
+# Duas formas de exponenciação em Python (abaixo)
+print(f'A raiz quadrada {num} é igual a: {num ** (1/2):.2f}')
+
+print(f'A raiz quadrada {num} é igual a: {pow(num,1/2):.2f}')
+
+#==========$$==========##==========$$==========##==========$$==========#
+
