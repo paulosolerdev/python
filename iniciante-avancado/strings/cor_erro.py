c493bed79b9dcc68e5aac28d133e20310b828bd5
Paulo Soler <paulosolerdev@gmail.com>
2024-07-25 09:33:58 -0300

    Create cor_erro.py

---
+++ iniciante-avancado/strings/cor_erro.py
@@ -0,0 +1,6 @@
+ERRO = '\033[91m'   # código de cores "vermelho"
+NORMAL = '\033[0m'
+MATRIX = '\033[1;32m'
+
+print(f'{ERRO}Mensagem de erro aqui. {NORMAL}Normal aqui.')
+print(f'{MATRIX}Mensagem da matrix aqui.')
