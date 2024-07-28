b46c5eed8ac8e5c581d9d963a5c2454262a26892
paulosolerdev <paulosolerdev@gmail.com>
2024-07-28 15:17:21 -0300

    mudança inicial do commit em python

---
+++ iniciante-avancado/pilhas/pilha.py
@@ -0,0 +1,10 @@
+pilha = [10, 20, 30]
+print(pilha)
+
+pilha.append(50)
+print(pilha)
+print(f"Esta pilha possui {len(pilha)} elementos.")
+
+pilha.pop()
+print(pilha)
+print(f"Esta pilha contém {len(pilha)} elementos.")
