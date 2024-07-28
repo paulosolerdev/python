dba24101206cdd01a58ff0620991f136bc0aa7f3
paulosolerdev <paulosolerdev@gmail.com>
2024-07-28 15:42:59 -0300

    atualização de dicionários

--- iniciante-avancado/dicionarios/inicio.py
+++ iniciante-avancado/dicionarios/inicio.py
@@ -4,3 +4,13 @@ paulo = {'nome': 'Paulo Soler',
          'Profissional Linux']}
 
 print(paulo)
+print(f"Há {len(paulo)} pares chave:valor neste dicionário.")
+print(f'A idade {paulo.get('nome')} é de {paulo.get('idade')} anos.')
+
+print(f'Há {len(paulo)} chaves neste dicionário, e elas são: \n\t{paulo.keys()}')
+print(f'E os valores são: \n\t{paulo.values()}')
+
+print(f'{paulo.items()}')
+
+for chave, valor in paulo.items():
+    print(f'Chave: {chave}, Valor: {valor}')
