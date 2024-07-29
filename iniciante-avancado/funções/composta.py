1550833f28ba904ab70e1036bd6da9afc985ff9f
paulosolerdev <paulosolerdev@gmail.com>
2024-07-29 17:07:10 -0300

    função composta

--- iniciante-avancado/funções/composta.py
+++ iniciante-avancado/funções/composta.py
@@ -1,4 +1,9 @@
 #Função composta(com parâmetros)
 
 def eleva_numero_ao_cubo(num):
-    
+    valor_a_retornar = num * num * num
+    return(valor_a_retornar)
+
+num = int(input('Informe um número que queira elevar ao cubo: '))
+num = eleva_numero_ao_cubo(num)
+print(num)
