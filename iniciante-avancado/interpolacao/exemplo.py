b72b10d6fa6c55e371b7f4d120ceb1d8b8ca87c7
paulosolerdev <paulosolerdev@gmail.com>
2024-07-29 16:41:49 -0300

    interpolaçoes

---
+++ iniciante-avancado/interpolacao/exemplo.py
@@ -0,0 +1,7 @@
+nome = input('Digite o seu nome: ')
+print(f"Seja bem-vindo {nome}")
+
+nome1 = 'Maria'
+idade = 30
+print(f'Nome: {nome1}, Idade: {idade}')
+print(f'{nome1} tem 3 vezes a minha idade, ela tem {3*idade} anos.')
