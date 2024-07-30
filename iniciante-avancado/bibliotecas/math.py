759e9050d95879542ff4ff021810ef4941e141b0
paulosolerdev <paulosolerdev@gmail.com>
2024-07-30 13:26:51 -0300

    apenas importando o que interessa ao código

--- iniciante-avancado/bibliotecas/math.py
+++ iniciante-avancado/bibliotecas/math.py
@@ -1,9 +1,13 @@
+# Importando math inteira
 # import math
 
 # raio = 15.3
 
 # print(f'Área do círculo, {math.pi * raio ** 2:.2f}')
 
+#======================================================#
+
+# Importando apenas pi (o que importa no código)
 from math import pi
 
 raio = 15.3
