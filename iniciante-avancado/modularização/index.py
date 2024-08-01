9fbb227a7e58f094f9d4dfca129c64f7dacc0b3d
paulosolerdev <paulosolerdev@gmail.com>
2024-08-01 09:19:41 -0300

    soma 2

--- iniciante-avancado/modularização/index.py
+++ iniciante-avancado/modularização/index.py
@@ -1,3 +1,7 @@
-import soma
+# import soma as sm
 
-print(f'O resultado da soma é: {soma.soma(0,0)}')
+# print(f'O resultado da soma é: {sm.calculo_soma(0,0)}')
+
+from soma import calculo_soma
+
+print(f'O resultado da soma é: {calculo_soma(0,0)}')
--- iniciante-avancado/modularização/soma.py
+++ iniciante-avancado/modularização/soma.py
@@ -1,4 +1,4 @@
-def soma(num1, num2):
+def calculo_soma(num1, num2):
     num1 = input('Digite um número: ')
     num2 = input('Digite outro número: ')
     s = int(num1) + int(num2)
