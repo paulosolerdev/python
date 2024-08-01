25ed1a4ca60a3f85d30adb538c6f7ede1d190d8d
paulosolerdev <paulosolerdev@gmail.com>
2024-08-01 10:21:42 -0300

    atualizar

--- iniciante-avancado/modularização/index.py
+++ iniciante-avancado/modularização/index.py
@@ -2,6 +2,6 @@
 
 # print(f'O resultado da soma é: {sm.calculo_soma(0,0)}')
 
-from soma import calculo_soma
+# from soma import calculo_soma
 
-print(f'O resultado da soma é: {calculo_soma(0,0)}')
+# print(f'O resultado da soma é: {calculo_soma(0,0)}')
