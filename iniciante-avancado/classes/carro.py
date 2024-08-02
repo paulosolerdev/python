aeb9d3c00fc2e2d078bc36f8a5c692d67d01ad24
paulosolerdev <paulosolerdev@gmail.com>
2024-08-02 07:11:16 -0300

    classes

---
+++ iniciante-avancado/classes/carro.py
@@ -0,0 +1,7 @@
+class Carro:
+    carro1 = 'Gol modelo 2016 completo'
+    carro2 = 'Celta ano 2015 4 portas'
+    carro3 = 'Uno ano 2015 baixa quilometragem'
+    carro4 = 'Clio 2018 flex doc vencido'
+
+print(Carro.carro1)
---
+++ iniciante-avancado/classes/pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    pass
+
+pessoa1 = Pessoa()
+
+pessoa1.nome = 'Paulo'
+pessoa1.idade = 39
+
+print(pessoa1.nome)
+print(pessoa1.idade)
