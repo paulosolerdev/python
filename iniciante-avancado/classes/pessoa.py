c57d5231e90ff1e6257b945989f2b5b8d42cee1a
paulosolerdev <paulosolerdev@gmail.com>
2024-08-02 07:21:19 -0300

    classe Pessoa

--- iniciante-avancado/classes/pessoa.py
+++ iniciante-avancado/classes/pessoa.py
@@ -1,10 +1,20 @@
+# class Pessoa:
+#     pass
+
+# pessoa1 = Pessoa()
+
+# pessoa1.nome = 'Paulo'
+# pessoa1.idade = 39
+
+# print(pessoa1.nome)
+# print(pessoa1.idade)
+
+#====================================#
+
 class Pessoa:
-    pass
+    def acao1(self):
+        print('Ação 1 está sendo executada...')
 
 pessoa1 = Pessoa()
 
-pessoa1.nome = 'Paulo'
-pessoa1.idade = 39
-
-print(pessoa1.nome)
-print(pessoa1.idade)
+pessoa1.acao1()
