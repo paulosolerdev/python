3ddd3be3f37aa5ce989d3d1d2c1d598cb6d53bad
paulosolerdev <paulosolerdev@gmail.com>
2024-08-05 18:44:07 -0300

    strings

---
+++ iniciante-avancado/strings/string.py
@@ -0,0 +1,26 @@
+# # Sintaxe básica de uma string
+
+# frase = 'Atributo em formato alfanumérico...'
+# frase2 = 'Incluindo números e símbolos!!!'
+
+# print(frase)
+# print(frase2)
+
+#===============================================#
+
+# # Contando quantos caracteres compõe uma string
+
+# frase1 = 'Porto Alegre é uma cidade brasileira.'
+
+# print(len(frase1))
+# print(len(frase1) - frase1.count(' '))
+
+#===============================================#
+
+# Substituindo elementos de uma string
+
+frase = 'Porto Alegre é uma cidade brasileira'
+
+frase = frase.replace('Porto Alegre', 'Curitiba')
+
+print(frase)
