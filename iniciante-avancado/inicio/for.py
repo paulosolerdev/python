d203130a600c34c070fcd0d1ee2f3fc4f039f762
paulosolerdev <paulosolerdev@gmail.com>
2024-08-05 08:42:50 -0300

    for

---
+++ iniciante-avancado/inicio/for.py
@@ -0,0 +1,15 @@
+# Estrutura de Repetição
+
+# FOR
+
+# compras = ['Arroz', 'Feijão', 'Massa', 'Carne', 'Pão']
+
+# for i in compras:
+#     print(i)
+
+#========================================================#
+
+nome = 'Soler'
+
+for i in nome:
+    print(i)
