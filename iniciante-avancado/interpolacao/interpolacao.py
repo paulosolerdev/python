4bf18848a0744628015c913dbd763eeaff12eb8d
paulosolerdev <paulosolerdev@gmail.com>
2024-08-07 09:52:35 -0300

    interpolacao

---
+++ iniciante-avancado/interpolacao/interpolacao.py
@@ -0,0 +1,16 @@
+# Uso de máscaras de substituição
+# INTERPOLAÇÃO
+
+nome = 'Maria'
+exp = 33
+
+print(f'Olá {nome}, queremos te parabenizar por seus {exp} anos em nossa empresa.')
+
+#================================================#
+
+# Operações dentro de máscaras de substituição
+
+nome = 'Maria'
+idade = 30
+
+print(f'{nome} tem 3 vezes minha idade, ela tem {3 * idade} anos!')
