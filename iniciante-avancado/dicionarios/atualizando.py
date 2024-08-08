e5f07e367fa50848eacadc45c9c4f8d9c3321022
paulosolerdev <paulosolerdev@gmail.com>
2024-08-08 08:13:01 -0300

    atualizando e removendo

---
+++ iniciante-avancado/dicionarios/atualizando.py
@@ -0,0 +1,26 @@
+# Atualizando um elemento do dicionário
+
+# d4 = {'1':'A',
+#       '2':'B',
+#       '3':'C',
+#       '4':'D'}
+
+# print(d4)
+
+# d4.update({'2':'E'})
+
+# print(d4)
+
+
+# Removendo um elemento do dicionário
+
+d5 = {'1':'A',
+      '2':'B',
+      '3':'C',
+      '4':'D'}
+
+print(d5)
+
+del d5['4']
+
+print(d5)
