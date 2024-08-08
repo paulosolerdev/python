099a092c2b9d601e5801fdcc2adaaab5fb48d5be
paulosolerdev <paulosolerdev@gmail.com>
2024-08-08 08:04:59 -0300

    pesquisa de elementos em dicionários

---
+++ iniciante-avancado/dicionarios/pesquisa.py
@@ -0,0 +1,23 @@
+# Verificando se uma chave ou valor consta em um dicionário
+
+# Pesquisando pela chave, obtendo valor
+
+d3 = {'1':'Ana',
+      '2':'Maria',
+      '3':'Paulo',
+      '4':'Susana'}
+
+# print(d3.get('2'))
+
+# print(d3.get('4'))
+
+# print(d3.get('5'))
+
+# print('3' in d3)
+
+# print('2' in d3.keys())
+
+# print('Ana' in d3.values())
+# print('Ana' in d3.keys())
+
+print(d3['2'] == 'Maria')
