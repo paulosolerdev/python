bc952b27fe03909b87db32c322c6b176bb886dd2
paulosolerdev <paulosolerdev@gmail.com>
2024-08-08 14:40:43 -0300

    listas em dicionário

---
+++ iniciante-avancado/dicionarios/listas.py
@@ -0,0 +1,9 @@
+# Listas como valores de uma chave do dicionário
+
+dict = {'almox':['Folha de Ofício', 'Caneta', 'Grampeador'],
+        'cozinha':['Café', 'Açúcar']}
+
+print(dict['almox'])
+print(dict['almox'][0])
+
+print(dict['cozinha'][1])
