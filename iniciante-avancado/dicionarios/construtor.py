3e01b8666b1dbba33a9540a6730d1f19dafe1bcd
paulosolerdev <paulosolerdev@gmail.com>
2024-08-07 22:23:42 -0300

    consultando valores em dicionário

---
+++ iniciante-avancado/dicionarios/construtor.py
@@ -0,0 +1,21 @@
+# # Usando o construtor de dicionários do Python
+
+# dicionario = dict(chave1 = 'valor da chave 1',
+#                   chave12 = 'valor da chave 2')
+
+# print(dicionario)
+
+# print(type(dicionario))
+
+#====================================#
+
+# Consultando apenas as chaves de um dicionário
+
+dicionario = dict(chave1 = 'valor da chave 1',
+                  chave2 = 'valor da chave 2')
+
+print(dicionario.keys())
+
+print(dicionario)
+
+print(dicionario.values())
