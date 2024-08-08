29e21a4197ea1f117d2f626e928cb8cd840d861b
paulosolerdev <paulosolerdev@gmail.com>
2024-08-07 21:37:25 -0300

    dicionarios

---
+++ iniciante-avancado/dicionarios/dicionario.py
@@ -0,0 +1,44 @@
+# # Sintaxe básica de um dicionário
+
+# dicionario = {'chave':'valor'}
+
+# print(dicionario)
+
+#============================================#
+
+# Adicionando novos elementos ao dicionário
+
+# dicionario = {'Nome1':'Paulo'}
+
+# print(dicionario)
+
+# dicionario['Nome2'] = 'Veronica'
+
+# print(dicionario)
+
+
+#============================================#
+
+# Alterando o valor de uma chave do dicionário
+
+# dicionario = {'Nome1':'Ana',
+#               'Nome2':'Carla',
+#               'Nome3':'Maria'}
+
+# print(dicionario)
+
+# dicionario['Nome1'] = 'Bárbara'
+
+# print(dicionario)
+
+#============================================#
+
+# Acessando um elemento do dicionário
+
+dicionario = {'Nome': 'Paulo',
+              'Idade': 39,
+              'Sexo':'Masculino',
+              'Nacionalidade':'Brasileira'}
+
+print(dicionario['Nome'])
+print(dicionario['Nacionalidade'])
