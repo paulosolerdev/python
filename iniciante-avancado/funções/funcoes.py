df76466795925ac791cd4504758688c71c35b40f
paulosolerdev <paulosolerdev@gmail.com>
2024-08-10 08:50:38 -0300

    estudando funções

---
+++ iniciante-avancado/funções/funcoes.py
@@ -0,0 +1,51 @@
+# FUNÇÕES
+
+# Sintaxe básica de uma função
+
+# def nome_da_funcao2(parametros):
+#     'corpo da função'
+
+# def minha_funcao():
+#     print('Minha primeira função personalizada!')
+
+# variavel = minha_funcao()
+
+#===============================================#
+
+# Definindi uma função sem parâmetros
+
+# def exibe_mensagem():
+#     print('Seja bem-vindo!')
+#     # poderia ser, dependendo do contexto, return 'Seja bem-vindo'!
+
+# print(exibe_mensagem())
+
+# msg = exibe_mensagem()
+
+#===============================================#
+
+# Chamando a função
+
+# def mensagem():
+#     print('Seja bem-vindo!')
+
+# mensagem()
+
+#===============================================#
+ # Função associada a uma variável
+
+# def mensagem():
+#     print('Seja bem-vindo!')
+
+# mensagem1 = mensagem()
+
+# print(mensagem1)
+
+#=======================#
+
+def mensagem():
+    return 'Seja bem-vindo!'
+
+mensagem1 = mensagem()
+
+print(mensagem1)
