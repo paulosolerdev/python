3ea5505f400cffe032f9b442b891ec8a12928e67
paulosolerdev <paulosolerdev@gmail.com>
2024-08-11 23:00:55 -0300

    recursividade

---
+++ iniciante-avancado/funções/recursividade.py
@@ -0,0 +1,17 @@
+# def imprimir(maximo, atual):
+#     if atual >= maximo:
+#         return
+#     print(atual)
+#     imprimir(maximo, atual+1)
+
+# imprimir(10, 1)
+
+
+#======================================#
+
+def imprimir(maximo, atual):
+    if atual < maximo:
+        print(atual)
+        imprimir(maximo, atual+1)
+
+imprimir(10, 1)
