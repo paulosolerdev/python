70b92bdb8121fff8ca2965e011114f51a7e02b5f
paulosolerdev <paulosolerdev@gmail.com>
2024-08-17 23:42:54 -0300

    exercícios de 6 a 10

---
+++ exercicios/ex_006.py
@@ -0,0 +1,4 @@
+num1 = 1985
+
+print(f'A variável num1 é do tipo: {type(num1)}')
+print(f'O valor de num1 é igual a: {num1}')
---
+++ exercicios/ex_009.py
@@ -0,0 +1,6 @@
+nomes = ['Ana', 'Susana', 'Daiane', 'Paulo', 'Maria']
+
+print(f'Essa é a lista nomes: {nomes}')
+print(f'Este é o tipo de dados: {type(nomes)}')
+print(f'E este é o tamanho da lista: {len(nomes)} elementos')
+print(f'Este é o elemento na posição 3: {nomes[3]}')
