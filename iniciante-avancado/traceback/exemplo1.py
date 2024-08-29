69e54fa9ab999d8f622f388412f1661b1b2c433f
paulosolerdev <paulosolerdev@gmail.com>
2024-08-29 14:41:48 -0300

    try e except

--- iniciante-avancado/traceback/exemplo1.py
+++ iniciante-avancado/traceback/exemplo1.py
@@ -1,6 +1,17 @@
-num1 = 13
-num2 = ad
+# num1 = 13
+# num2 = ad
 
-soma = num1 + num2
+# soma = num1 + num2
 
-print(soma)
+# print(soma)
+
+while True:
+    try:
+        num1 = int(input('Digite o primeiro número: '))
+        num2 = int(input('Digite o segundo número: '))
+        soma = num1 + num2
+        print(f'O resultado da soma é: {soma}')
+        break
+    
+    except ValueError:
+        print('Número inválido, tente novamente. ')
