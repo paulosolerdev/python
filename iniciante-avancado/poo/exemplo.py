1f0caa133ff6d77f476b6bcc150d98f3ebecd4e3
paulosolerdev <paulosolerdev@gmail.com>
2024-08-30 12:30:08 -0300

    início a POO

---
+++ iniciante-avancado/poo/exemplo.py
@@ -0,0 +1,12 @@
+# pessoa1 = {'Nome':'Paulo', 'Idade':39}
+# print(pessoa1)
+
+class Pessoa:
+    pass
+
+pessoa1 = Pessoa()
+pessoa1.Nome = 'Paulo'
+pessoa1.Idade = 39
+pessoa1.Documentos = {'Identidade':444444444,
+                      'CPF':9999999999}
+pessoa1.Pais = ['Paulo Sergio Soler', 'Silvia Regina dos Santos Soler']
