b81e0e947cdea4214789cdb1298d43df6f73fea3
paulosolerdev <paulosolerdev@gmail.com>
2024-09-12 08:32:55 -0300

    sintaxe de programação orientada a objetos

---
+++ iniciante-avancado/poo/sintaxe.py
@@ -0,0 +1,25 @@
+# Criando objetos (atributos de classe) dentro de uma classe
+
+# class Pessoa:
+#     pass
+
+# pessoa1 = Pessoa()
+
+
+# pessoa1.nome = 'Paulo Soler'
+# pessoa1.idade = 39
+
+
+# print(pessoa1.nome)
+
+
+#===========================================#
+
+
+class Pessoa:
+    def acao1(self):
+        print('Ação 1 sendo executada...')
+
+pessoa1 = Pessoa()
+
+pessoa1.acao1()
