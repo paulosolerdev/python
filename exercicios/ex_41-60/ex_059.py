4bd02da112ad518e7f6c7a7425b17b5951cdfd38
paulosolerdev <paulosolerdev@gmail.com>
2024-09-23 11:07:16 -0300

    exercícios de parâmetros de função, incluindo *args

---
+++ exercicios/ex_41-60/ex_059.py
@@ -0,0 +1,8 @@
+def pessoa(nome, idade=39, funcao='técnico'):
+    print(f'{nome} tem {idade} anos, e sua função é {funcao}.')
+
+p1 = pessoa('Paulo', funcao='programador')
+
+p2 = pessoa('Carlos', idade=50)
+
+p3 = pessoa('Jussara')
---
+++ exercicios/ex_41-60/ex_060.py
@@ -0,0 +1,4 @@
+def msg(*args):
+    print(f'Os parâmetros são: {args}')
+
+ex2 = msg('nome = Paulo', 'idade = 39', 'profissão = programador')
