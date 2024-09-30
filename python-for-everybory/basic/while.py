a2e9a1f618bfa455d366454db292af02a5e8257b
paulosolerdev <paulosolerdev@gmail.com>
2024-09-30 10:13:33 -0300

    whiles

--- python-for-everybory/basic/while.py
+++ python-for-everybory/basic/while.py
@@ -1,9 +1,14 @@
 
-while True:
-    line = input('> ')
-    if line[0] == '#':
-        continue
-    if line == 'done':
-        break
-    print(line)
-print('Done!')
+# while True:
+#     line = input('> ')
+#     if line[0] == '#':
+#         continue
+#     if line == 'done':
+#         break
+#     print(line)
+# print('Done!')
+
+
+#===================================#
+
+
