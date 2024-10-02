4b5b059be0e44aa5088d1f477b17c9dae71f1418
paulosolerdev <paulosolerdev@gmail.com>
2024-10-02 15:15:10 -0300

    de volta às classes básicas

---
+++ iniciante-avancado/classes/pessoa2.py
@@ -0,0 +1,13 @@
+class Pessoa:
+    ano_atual = 2024
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def ano_nascimento(self):
+        ano_nasc = self.ano_atual - self.idade
+        print(f'Seu ano de nascimento é {ano_nasc}')
+
+pessoa1 = Pessoa('Paulo Soler', 39)
+print(pessoa1.ano_nascimento())
