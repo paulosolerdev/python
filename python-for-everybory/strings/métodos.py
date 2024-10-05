165813951109e86eb978a8a382fb10229ae5d355
paulosolerdev <paulosolerdev@gmail.com>
2024-10-05 08:53:40 -0300

    métodos de strings

---
+++ python-for-everybory/strings/métodos.py
@@ -0,0 +1,51 @@
+# fruit = 'banana'
+# pos = fruit.find('na')
+# print(pos)
+
+# aa = fruit.find('z')
+# print(aa)
+
+
+#=====================================#
+
+
+# greet = 'Hello Jr.'
+# nnn = greet.upper()
+# print(nnn)
+
+# www = greet.lower()
+# print(www)
+
+
+#=====================================#
+
+
+
+# greet = 'Hello Paulo'
+# nstr = greet.replace('Paulo', 'Susana')
+# print(nstr)
+
+# nstr = greet.replace('o', 'X')
+# print(nstr)
+
+
+#=====================================#
+
+
+
+# greet = '   Hello Paulo   '
+# print(greet.lstrip())
+
+# print(greet.rstrip())
+
+# print(greet.strip())
+
+
+#=====================================#
+
+
+
+line = 'Please have a nice day'
+print(line.startswith('Please'))
+
+print(line.startswith('p'))
