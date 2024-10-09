8bc73c5e87188bf1d88a4720303fd486b548394d
paulosolerdev <paulosolerdev@gmail.com>
2024-10-09 14:11:33 -0300

    lendo arquivos em python

--- python-for-everybory/strings/reading_files.py
+++ python-for-everybory/strings/reading_files.py
@@ -1,3 +1,40 @@
+# fhand = open('arquivo-texto.txt')
+
+# print(fhand)
+
+#=====================================#
+
+# xfile = open('arquivo-texto.txt')
+# for cheese in xfile:
+#     print(cheese)
+
+
+#=====================================#
+
+
+
+# fhand = open('arquivo-texto.txt')
+# count = 0
+# for line in fhand:
+#     count = count + 1
+# print(f'Line Count: {count}')
+
+
+#=====================================#
+
+
+# fhand = open('arquivo-texto.txt')
+# inp = fhand.read()
+# print(len(inp))
+
+# print(inp[:101])
+
+
+#=====================================#
+
+
 fhand = open('arquivo-texto.txt')
 
-print(fhand)
+for line in fhand:
+    if line.startswith('From:'):
+        print(line)
