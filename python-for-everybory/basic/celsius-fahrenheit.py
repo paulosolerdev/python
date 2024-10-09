73917d8affccc323d86ca8cb4768836fbbe38b7d
paulosolerdev <paulosolerdev@gmail.com>
2024-10-09 14:57:51 -0300

    try-except

--- python-for-everybory/basic/celsius-fahrenheit.py
+++ python-for-everybory/basic/celsius-fahrenheit.py
@@ -2,8 +2,20 @@
 
 # F = (C * 9/5) + 32
 
-c = float(input('Digite a temperatura em Celsius: '))
+# c = float(input('Digite a temperatura em Celsius: '))
 
-f = (c * 9/5) + 32
+# f = (c * 9/5) + 32
 
-print(f'{c} graus Celsius equivale a {f} graus fahrenheit.')
+# print(f'{c} graus Celsius equivale a {f} graus fahrenheit.')
+
+
+#=====================================#
+
+
+inp = input('Enter Fahrenheit Temperature: ')
+try:
+    fahr = float(inp)
+    cel = (fahr - 32.0) * 5.0 / 9.0
+    print(cel)
+except:
+    print('Please enter a number')
