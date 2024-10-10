e858df61b37a93ff18a006a15345d2d952f98257
paulosolerdev <paulosolerdev@gmail.com>
2024-10-10 09:33:19 -0300

    listas

---
+++ python-for-everybory/listas/listas.py
@@ -0,0 +1,10 @@
+friends = ['João', 'Maria', 'Pedro']
+
+for friend in friends:
+    print(f'Happy New Year {friend}!')
+
+print(f'$============================$')
+
+for i in range(len(friends)):
+    friend = friends[i]
+    print(f'Happy New Year: {friend}')
