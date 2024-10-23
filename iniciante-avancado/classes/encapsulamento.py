eed2997b9520511f1337ddfc6618ddfccba40aa8
paulosolerdev <paulosolerdev@gmail.com>
2024-10-23 07:54:00 -0300

    Encapsulamento de Classe

--- iniciante-avancado/classes/encapsulamento.py
+++ iniciante-avancado/classes/encapsulamento.py
@@ -1,19 +1,19 @@
 class BaseDeDados:
     def __init__(self):
-        self.base = {}
+        self.__base = {}
     
     def inserir(self, nome, fone):
-        if 'clientes' not in self.base:
-            self.base['clientes'] = {nome:fone}
+        if 'clientes' not in self.__base:
+            self.__base['clientes'] = {nome:fone}
         else:
-            self.base['clientes'].update({nome:fone})
+            self.__base['clientes'].update({nome:fone})
 
     def listar(self):
-        for nome, fone in self.base['clientes'].items():
+        for nome, fone in self.__base['clientes'].items():
             print(nome, fone)
 
     def apagar(self, nome):
-        del self.base['clientes'][nome]
+        del self.__base['clientes'][nome]
 
 relClientes = BaseDeDados()
 
@@ -22,3 +22,6 @@ relClientes.inserir('Fernando', 981252001)
 relClientes.inserir('Maria', 999111415)
 
 relClientes.listar()
+relClientes.__base = 'Novo Banco de Dados'
+print(relClientes.__base)
+print(relClientes._BaseDeDados__base)
