827cf80fed3a851200e834a9e443943184a86a45
paulosolerdev <paulosolerdev@gmail.com>
2024-11-23 14:20:41 -0300

    lambda

---
+++ iniciante-avancado/lambda/lambda.py
@@ -0,0 +1,11 @@
+variavel1 = lambda n1, n2: n1 * n2
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+
+print(f"O primeiro número digitado foi: {n1}")
+print(f"O segundo número digitado foi: {n2}")
+
+
+print(f'A multiplicação por lambda é: {variavel1(n1, n2)}')
