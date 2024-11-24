b6400c1a322448cdc3d1cfd510794549b6083b25
paulosolerdev <paulosolerdev@gmail.com>
2024-11-24 08:31:48 -0300

    Exercício 98, Luiz Otávio

---
+++ luiz-otavio/exercicios/ex_98.py
@@ -0,0 +1,13 @@
+cpf = '15883166610'
+nove_digitos = cpf[:9]
+contador_regressivo = 10
+resultado = 0
+
+for digito in nove_digitos:
+    resultado += int(digito) * contador_regressivo
+    contador_regressivo -= 1
+
+digito = (resultado * 10) % 11
+digito = digito if digito <= 9 else 0
+
+print(digito)
