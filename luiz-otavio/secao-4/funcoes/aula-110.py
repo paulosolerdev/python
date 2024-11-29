37d3c0b530d43e12af4be2fbf99c8a425ca56abc
paulosolerdev <paulosolerdev@gmail.com>
2024-11-29 07:58:10 -0300

    return

---
+++ luiz-otavio/secao-4/funcoes/aula-110.py
@@ -0,0 +1,11 @@
+def soma(x, y):
+    return x + y
+
+
+soma1 = soma(2, 2)
+print(f'A soma de x + y = {soma1}')
+
+soma2 = soma(3, 3)
+print(f'A soma de x + y = {soma2}')
+
+print(f"A soma de soma1 + soma2 = {soma1+soma2}")
