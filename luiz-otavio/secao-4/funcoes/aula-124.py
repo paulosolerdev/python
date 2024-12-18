b04cf83ecd127e6c13ae7a75c98a8c0084fd6a94
paulosolerdev <paulosolerdev@gmail.com>
2024-12-17 22:26:59 -0300

    aula 124 do Luiz Otávio

---
+++ luiz-otavio/secao-4/funcoes/aula-124.py
@@ -0,0 +1,26 @@
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta':'25',
+    },
+
+    {
+        'Pergunta':'Quanto é 10/2?',
+        'Opções':['4', '5', '2', '1'],
+    },
+]
+
+
+for pergunta in perguntas:
+    print(pergunta['Pergunta'])
+    print()
+
+    for opcao in pergunta['Opções']:
+        print(opcao)
