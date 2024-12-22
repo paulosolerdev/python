5c4d6d89a362a58c60dec22121c5407ada0a4b53
paulosolerdev <paulosolerdev@gmail.com>
2024-12-22 20:16:42 -0300

    list comprehension

---
+++ iniciante-avancado/list-comprehension/exemplo1.py
@@ -0,0 +1,12 @@
+# lista1 = [1, 2, 3, 4, 5, 6]
+
+# for i in lista1:
+#     print(i * 2)
+
+# lista2 = [i * 2 for i in lista1]
+# print(lista2)
+
+string = '1234567890'
+comprehension = [letra for letra in string]
+
+print(comprehension)
