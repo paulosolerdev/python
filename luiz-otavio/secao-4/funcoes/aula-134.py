9b450af82468f7738fd22cfc1b7864b2e012da88
paulosolerdev <paulosolerdev@gmail.com>
2024-12-24 08:01:47 -0300

    lambda pra coisas curtas

---
+++ luiz-otavio/secao-4/funcoes/aula-134.py
@@ -0,0 +1,20 @@
+# def executa(funcao, *args):
+#     return funcao(*args)
+
+
+# def soma(x, y):
+#     return x+y
+
+
+# def cria_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero*multiplicador
+#     return multiplica
+
+
+# print(
+#     executa(
+#         lambda x, y: x+y
+#     ),
+#     executa(soma)
+# )
